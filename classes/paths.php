5e2c5c476dfc11f91b87793b843c87f45b354040
makingoff <mail@makingoff.name>
2016-01-02 02:15:37 +0300

    Adds preparing function at paths

--- classes/paths.php
+++ classes/paths.php
@@ -1,11 +1,34 @@
 <?php if (!defined('ROOT')) die('You can\'t just open this file, dude');
 
+  define('PPD_OPEN_LEFT', 1 << 0); // 0001
+  define('PPD_CLOSE_LEFT', 1 << 1); // 0010
+  define('PPD_OPEN_RIGHT', 1 << 2); // 0100
+  define('PPD_CLOSE_RIGHT', 1 << 3); // 1000
+  define('PPD_DEFAULT', PPD_CLOSE_LEFT | PPD_CLOSE_RIGHT); // 1000
+
   class SFPath
   {
     private static $s3 = false;
     private static $awsAccessKey;
     private static $awsSecretKey;
     private static $bucket;
+
+    public static function prepareDir($path, $conf = PPD_DEFAULT)
+    {
+      if (substr($path, 0, 1) === '/' && ($conf & PPD_OPEN_LEFT)) {
+        $path = substr($path, 1);
+      }
+      if (substr($path, 0, 1) !== '/' && ($conf & PPD_CLOSE_LEFT)) {
+        $path = '/' . $path;
+      }
+      if (substr($path, -1) === '/' && ($conf & PPD_OPEN_RIGHT)) {
+        $path = substr($path, 0, -1);
+      }
+      if (substr($path, -1) !== '/' && ($conf & PPD_CLOSE_RIGHT)) {
+        $path .= '/';
+      }
+      return $path;
+    }
 
     public static function mkdir($path)
     {
