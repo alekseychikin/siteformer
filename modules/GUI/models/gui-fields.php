3395106e77aa76fbba52aae45383904ba19021ff
makingoff <mail@makingoff.name>
2016-11-20 13:17:57 +0300

    Fixes get fields

--- modules/GUI/models/gui-fields.php
+++ modules/GUI/models/gui-fields.php
@@ -8,6 +8,14 @@ class SFGuiFields extends SFRouterModel
     } else {
       $section = SFGUI::getSection($params['section']);
 
+      if (isset($params['limit'])) {
+        $fields = arrSort($section['fields'], function ($item1, $item2) {
+          return $item1['position'] < $item2['position'];
+        });
+
+        return array_slice($fields, 0, $params['limit']);
+      }
+
       return $section['fields'];
     }
 
