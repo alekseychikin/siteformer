9d3ef3ac25fdbefe7a3e63a63369fb3cb8ed3b5d
Aleksei Chikin <mail@makingoff.name>
2017-04-19 22:28:38 +0300

    Adds some tests for classes/response.php

--- classes/response.php
+++ classes/response.php
@@ -2,12 +2,9 @@
 
 class SFResponse
 {
-  private static $result = [];
-  private static $toGlobal = [];
+  private static $state = [];
   private static $template;
   private static $main;
-  private static $propagation = true;
-  private static $working = true;
   private static $isShowContent = false;
   private static $code = [
     '200' => 'OK',
@@ -18,22 +15,11 @@ class SFResponse
     '422' => 'Unprocessable Entity',
     '500' => 'Internal Server Error'
   ];
-  private static $stopsCodes = [
-    '400', '401', '402', '403', '404', '422', '500'
-  ];
   private static $error = 200;
 
-  public static function code($code) {
-    self::$error = $code;
-    header('HTTP/1.1 ' . $code . ' ' . self::$code[$code]);
-  }
-
-  public static function isWorking() {
-    return self::$working;
-  }
-
-  public static function error($status, $message, $file, $line, $trace = false) {
-    self::code($status);
+  public static function error($code, $message, $file = '', $line = '', $trace = false) {
+    self::$code = $code;
+    header('HTTP/1.1 ' . $code . ' ' . self::$codes[$code]);
 
     if ($trace === false) {
       $error = new Exception();
@@ -55,69 +41,36 @@ class SFResponse
   }
 
   public static function setStatus($code) {
-    header('HTTP/1.1 ' . $code . ' ' . self::$code[$code]);
+    header('HTTP/1.1 ' . $code . ' ' . self::$codes[$code]);
   }
 
   public static function initRedirData() {
     if (isset($_SESSION['redir_data'])) {
-      self::$result = array_merge(self::$result, $_SESSION['redir_data']);
+      self::$state = array_merge_recursive(self::$state, $_SESSION['redir_data']);
       unset($_SESSION['redir_data']);
     }
   }
 
-  public static function isPropagation() {
-    return self::$propagation;
-  }
-
-  public static function stopPropagation() {
-    self::$propagation = false;
-  }
-
-  public static function set($name, $result, $toGlobal = false) {
-    if ($toGlobal) {
-      self::$toGlobal[$name] = $result;
-    }
-
-    self::$result[$name] = $result;
+  public static function set($field, $value) {
+    self::$state[$field] = $value;
   }
 
   public static function get($name) {
-    if (isset(self::$result[$name])) {
-      return self::$result[$name];
+    if (isset(self::$state[$name])) {
+      return self::$state[$name];
     }
 
     return false;
   }
 
-  public static function getGlobal() {
-    return self::$toGlobal;
-  }
-
-  private static function prepareActionPath($action, & $isFile, & $isDir) {
-    if (substr($action, -1) === '/') {
-      $action = substr($action, 0, -1);
-      $isDir = true;
-    }
-
-    if (substr($action, -4) === '.php') {
-      $action = substr($action, 0, -4);
-      $isFile = true;
-    }
-
-    return $action;
-  }
-
   public static function run($action, $params = []) {
-    if (!self::$working) return false;
-
     $urlParams = [];
 
     foreach ($params as $field => $value) {
       $$field = $value;
-      // $urlParams[] = '<input type="hidden" id="url_param_'. $field .'" role="url_param_'. $field .'" value="'. $value .'" />';
     }
 
-    SFResponse::set('url_params', implode(N, $urlParams));
+    self::set('url_params', implode(N, $urlParams));
     $actionsPath = ROOT;
 
     if (defined('ACTIONS')) {
@@ -143,8 +96,6 @@ class SFResponse
   }
 
   public static function actionExists($action) {
-    if (!self::$working) return false;
-
     $actionsPath = ROOT;
 
     if (defined('ACTIONS')) {
@@ -168,37 +119,12 @@ class SFResponse
     return false;
   }
 
-  public static function template($template) {
-    self::$template = $template;
+  public static function getState() {
+    return self::$state;
   }
 
-  public static function main($main) {
-    self::$main = $main;
-  }
-
-  public static function getResults() {
-    return self::$result;
-  }
-
-  private static function getRequestHeaders() {
-    $headers = array();
-
-    foreach($_SERVER as $key => $value) {
-      if (strtolower(substr($key, 0, 5)) !== 'http_') {
-        continue;
-      }
-
-      $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-      $headers[$header] = $value;
-    }
-
-    return $headers;
-  }
-
-  private static function returnData() {
+  public static function render() {
     global $startTime;
-
-    if (!self::$working) return false;
 
     $content = '';
 
@@ -216,14 +142,14 @@ class SFResponse
     $accept = 'text/html';
 
     if (isset($headers['Accept'])) {
-      $accept = $headers['Accept'];
+      $accept = strtolower($headers['Accept']);
     }
 
     switch ($accept) {
       case 'application/json':
         header('Content-Type: application/json; charset=utf-8');
 
-        echo json_encode(self::$result);
+        echo json_encode(self::$state);
 
         break;
       case 'text/xml':
@@ -231,43 +157,33 @@ class SFResponse
       case 'application/xhtml+xml':
         header('Content-Type: application/xml; charset=utf-8');
 
-        echo '<?xml version="1.0" ?><data>'.self::generateXML(self::$result).'</data>';
+        echo '<?xml version="1.0" ?><data>'.self::generateXML(self::$state).'</data>';
 
         break;
       default:
         if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
-          $_SESSION['redir_data'] = self::$result;
+          $_SESSION['redir_data'] = self::$state;
           self::redir($_SERVER['HTTP_REFERER']);
         } else {
-          if (self::$error === 200) {
+          if (self::$code === 200) {
             echo $content;
             $endTime = explode(' ', microtime());
             $endTime = $endTime[1] + $endTime[0];
 
             echo '<!-- ' . ($endTime - $startTime) . 's; ' . SFORMDatabase::$countQueries . ' queries -->';
           } else {
-            $trace = print_r(self::$result['error']['trace'], true);
-            echo '<pre>' . self::$result['error']['message'];
-            echo ' at ' . self::$result['error']['file'] . "\n";
+            $trace = print_r(self::$state['error']['trace'], true);
+            echo '<pre>' . self::$state['error']['message'];
+            echo ' at ' . self::$state['error']['file'] . "\n";
             echo $trace . '</pre>';
           }
         }
     }
 
     die();
-
-    self::$working = false;
-  }
-
-  public static function render() {
-    if (!self::$working) return false;
-
-    self::returnData();
   }
 
   public static function redir($path) {
-    if (!self::$working) return false;
-
     $_SESSION['location'] = $path;
     header('Location: /', true, 301);
 
@@ -275,13 +191,40 @@ class SFResponse
   }
 
   public static function refresh() {
-    if (!self::$working) return false;
-
     self::redir($_SERVER['REQUEST_URI']);
   }
 
   public static function showContent() {
     self::$isShowContent = true;
+  }
+
+  private static function prepareActionPath($action, & $isFile, & $isDir) {
+    if (substr($action, -1) === '/') {
+      $action = substr($action, 0, -1);
+      $isDir = true;
+    }
+
+    if (substr($action, -4) === '.php') {
+      $action = substr($action, 0, -4);
+      $isFile = true;
+    }
+
+    return $action;
+  }
+
+  private static function getRequestHeaders() {
+    $headers = array();
+
+    foreach($_SERVER as $key => $value) {
+      if (strtolower(substr($key, 0, 5)) !== 'http_') {
+        continue;
+      }
+
+      $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+      $headers[$header] = $value;
+    }
+
+    return $headers;
   }
 
   private static function generateXML($data) {
---
+++ tests/classes/responseTest.php
@@ -0,0 +1,80 @@
+<?php
+
+if (!defined('ROOT')) {
+  define('ROOT', __DIR__.'/../../');
+}
+
+session_start();
+
+require_once ROOT . 'classes/response.php';
+
+class ResponseTest extends PHPUnit_Framework_TestCase
+{
+  public function testSetValue() {
+    $this->assertEquals('', SFResponse::get('name'));
+    SFResponse::set('name', 'testname');
+    $this->assertEquals('testname', SFResponse::get('name'));
+
+    $this->assertEquals('', SFResponse::get('items'));
+    SFResponse::set('items', [1, 2, 3, 4, 5]);
+    $this->assertEquals([1, 2, 3, 4, 5], SFResponse::get('items'));
+  }
+
+  public function testExistsAction() {
+    $this->assertEquals(false, SFResponse::actionExists('action'));
+    $this->assertEquals(false, SFResponse::actionExists('action/index'));
+    $this->assertEquals(false, SFResponse::actionExists('action/'));
+    $this->assertEquals(false, SFResponse::actionExists('action/index/'));
+    $this->assertEquals(false, SFResponse::actionExists('action.php'));
+    $this->assertEquals(false, SFResponse::actionExists('action/index.php'));
+
+    $this->assertEquals(false, SFResponse::actionExists('/action'));
+    $this->assertEquals(false, SFResponse::actionExists('/action/index'));
+    $this->assertEquals(false, SFResponse::actionExists('/action/'));
+    $this->assertEquals(false, SFResponse::actionExists('/action/index/'));
+    $this->assertEquals(false, SFResponse::actionExists('/action.php'));
+    $this->assertEquals(false, SFResponse::actionExists('/action/index.php'));
+
+    $this->assertEquals(true, SFResponse::actionExists(ROOT . 'tests/action'));
+    $this->assertEquals(true, SFResponse::actionExists(ROOT . 'tests/action/index'));
+    $this->assertEquals(false, SFResponse::actionExists(ROOT . 'tests/action.php'));
+    $this->assertEquals(true, SFResponse::actionExists(ROOT . 'tests/action/index.php'));
+    $this->assertEquals(true, SFResponse::actionExists(ROOT . 'tests/action/'));
+    $this->assertEquals(false, SFResponse::actionExists(ROOT . 'tests/action/index/'));
+
+    $this->assertEquals(true, SFResponse::actionExists('tests/action'));
+    $this->assertEquals(true, SFResponse::actionExists('tests/action/index'));
+    $this->assertEquals(true, SFResponse::actionExists('tests/action/'));
+    $this->assertEquals(false, SFResponse::actionExists('tests/action/index/'));
+    $this->assertEquals(false, SFResponse::actionExists('tests/action.php'));
+    $this->assertEquals(true, SFResponse::actionExists('tests/action/index.php'));
+  }
+
+  public function testGetState() {
+    $this->assertEquals(['name' => 'testname', 'items' => [1, 2, 3, 4, 5]], SFResponse::getState());
+  }
+
+  public function testInitRedirData() {
+    $this->assertEquals([
+      'name' => 'testname',
+      'items' => [1, 2, 3, 4, 5]
+    ], SFResponse::getState());
+
+    $_SESSION['redir_data'] = ['redir_variable' => 'redir value'];
+
+    $this->assertEquals([
+      'name' => 'testname',
+      'items' => [1, 2, 3, 4, 5]
+    ], SFResponse::getState());
+
+    SFResponse::initRedirData();
+
+    $this->assertEquals([
+      'name' => 'testname',
+      'items' => [1, 2, 3, 4, 5],
+      'redir_variable' => 'redir value'
+    ], SFResponse::getState());
+
+    $this->assertEquals(false, isset($_SESSION['redir_data']));
+  }
+}
