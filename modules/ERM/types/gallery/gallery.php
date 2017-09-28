53cbf73ab9a2e7ae2904585646f5cfbd25ba9706
Aleksei Chikin <mail@makingoff.name>
2017-09-28 23:34:03 +0300

    Adds gallery adding photos

--- modules/ERM/types/gallery/gallery.php
+++ modules/ERM/types/gallery/gallery.php
@@ -5,29 +5,120 @@ require_once __DIR__ . '/../../ERMType.php';
 
 class SFTypeGallery extends SFERMType
 {
-  public static function getSqlField($params)
-  {
-    return array(
-      'type' => 'INT(11) UNSIGNED',
-      'default' => null
-    );
+  public static function hasSqlField() {
+    return false;
   }
 
-  public static function validateSettings($params, $fields, $currentAlias)
-  {
+  public static function validateSettings($params, $fields, $currentAlias) {
     return SFValidate::value([
       'storage' => [
-        'values' => ['local', 's3']
+        'values' => SFStorages::getStorageList(),
+        'required' => true
       ],
       'path' => [],
       'width' => [],
       'height' => [],
       'previewWidth' => [],
-      'previewHeight' => [],
-      's3AccessKey' => [],
-      's3SecretKey' => [],
-      's3Bucket' => [],
-      's3Path' => []
+      'previewHeight' => []
     ], $params);
+  }
+
+  public static function prepareDatabase() {
+    if (!SFORM::exists('sys_type_gallery')) {
+      SFORM::create('sys_type_gallery')
+        ->addField([
+          'name' => 'id',
+          'type' => 'INT(11) UNSIGNED',
+          'autoincrement' => true,
+          'null' => false
+        ])
+        ->addField([
+          'name' => 'collection',
+          'type' => 'INT(11) UNSIGNED',
+          'null' => 'NULL',
+          'default' => NULL
+        ])
+        ->addField([
+          'name' => 'field',
+          'type' => 'VARCHAR(100)',
+          'null' => 'NULL',
+          'default' => NULL
+        ])
+        ->addField([
+          'name' => 'record',
+          'type' => 'INT(11) UNSIGNED',
+          'null' => false
+        ])
+        ->addField([
+          'name' => 'preview',
+          'type' => 'VARCHAR(200)',
+          'null' => 'NULL',
+          'default' => NULL
+        ])
+        ->addField([
+          'name' => 'photo',
+          'type' => 'VARCHAR(200)',
+          'null' => 'NULL',
+          'default' => NULL
+        ])
+        ->addKey(['collection', 'field'])
+        ->addKey('id', 'primary key')
+        ->exec();
+    }
+  }
+
+  public static function postPrepareInsertData($collection, $field, $record, $data) {
+    $photos = $data[$field['alias']];
+    $settings = $field['settings'];
+
+    foreach ($photos as $photo) {
+      $basename = basename($photo);
+      $dirname = dirname($photo);
+      $preview = pathresolve($dirname, 'preview_' . $basename);
+
+      copy($photo, $preview);
+
+      SFImage::resize($preview, [
+        'width' => $settings['previewWidth'],
+        'height' => $settings['previewHeight']
+      ]);
+      SFImage::resize($photo, $settings);
+
+      $preview = SFStorages::put($settings['storage'], $preview, $settings['path']);
+      $photo = SFStorages::put($settings['storage'], $photo, $settings['path']);
+
+      SFORM::insert('sys_type_gallery')
+        ->values([
+          'collection' => $collection['id'],
+          'field' => $field['alias'],
+          'record' => $record['id'],
+          'preview' => $preview,
+          'photo' => $photo
+        ])
+        ->exec();
+    }
+  }
+
+  public static function joinData($databaseQuery, $collection, $field) {
+    $databaseQuery->join('sys_type_gallery')
+      ->on('sys_type_gallery.record', SFORM::field($collection['table'] . '.id'))
+      ->andOn('sys_type_gallery.collection', $collection['id'])
+      ->andOn('sys_type_gallery.field', $field['alias']);
+  }
+
+  public static function postProcessData($collection, $field, $data) {
+    $data[$field['alias']] = [];
+
+    foreach ($data['sys_type_gallery'] as $index => $item) {
+      if ($item['field'] === $field['alias']) {
+        unset($item['collection']);
+        unset($item['field']);
+        unset($item['record']);
+        $data[$field['alias']][] = $item;
+        unset($data['sys_type_gallery'][$index]);
+      }
+    }
+
+    return $data;
   }
 }
