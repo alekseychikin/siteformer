e3e615a9e107fbd0e13a1ffc3d19ea5518ca4318
Aleksei Chikin <mail@makingoff.name>
2018-01-11 22:14:42 +0300

    Adds setIn method to store

--- modules/GUI/src/libs/store.js
+++ modules/GUI/src/libs/store.js
@@ -16,6 +16,59 @@ export class Store {
 		}
 	}
 
+	setIn(path, changer) {
+		const object = this.getObjectByPath(path)
+		let changes = changer
+
+		if (typeof changer === Function) {
+				changes = changer(object)
+		}
+
+		Object.keys(changes).forEach(key => {
+			if (changes[key] === null || changes[key] === undefined) {
+				object[key] = changes[key]
+			} else if (changes[key].constructor === Array) {
+				object[key] = [ ...changes[key] ]
+			} else if (changes[key].constructor === Object) {
+				object[key] = { ...changes[key] }
+			} else {
+				object[key] = changes[key]
+			}
+		})
+
+		if (this.setStateHandler) {
+			this.setStateHandler(this)
+		}
+	}
+
+	getObjectByPath(path, updatePath = true) {
+		let parentLink = this.state
+
+		if (!path) {
+			return parentLink
+		}
+
+		const paths = path.split('.')
+
+		paths.forEach((key) => {
+			if (updatePath) {
+				if (!parentLink[key]) {
+					parentLink[key] = {}
+				} else if (parentLink[key].constructor === Array) {
+					parentLink[key] = [ ...parentLink[key] ]
+				} else if (parentLink[key].constructor === Object) {
+					parentLink[key] = { ...parentLink[key] }
+				}
+			}
+
+			if (typeof parentLink[key] === 'object') {
+				parentLink = parentLink[key]
+			}
+		})
+
+		return parentLink
+	}
+
 	getState() {
 		return { ...this.state }
 	}
