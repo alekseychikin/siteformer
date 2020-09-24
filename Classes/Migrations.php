0f8bed15247b55c8e357e29a17c1ba29fd855257
Aleksei Chikin <mail@makingoff.name>
2020-09-24 15:59:17 +0300

    Fixes migrations

--- Classes/Migrations.php
+++ Classes/Migrations.php
@@ -3,6 +3,8 @@
 namespace Engine\Classes;
 
 class Migrations {
+	private static $migrationFile = 'migrations.json';
+	private static $migrationLock = 'migrations.lock';
 	private static $migrations = [];
 	private static $pathMigrations;
 
@@ -13,26 +15,24 @@ class Migrations {
 			die('Не найдена папка с миграциями: ' . self::$pathMigrations);
 		}
 
-		self::preserveLastMigration();
-
-		if (isset($_GET['migration']) && !empty($_GET['migration']) && APPLICATION_ENV === 'develop') {
-			self::doMigration($_GET['migration']);
-		}
-
-		if (isset($_GET['migrations'])) {
-			self::doMigrations();
-		}
+		self::migration();
 	}
 
-	private static function doMigrations() {
-		$lockFile = pathresolve(self::$pathMigrations, 'migrations.lock');
+	private static function migration() {
+		self::preserveLastMigration();
+
+		if (file_exists(pathresolve(self::$pathMigrations, self::$migrationLock))) {
+			return false;
+		}
+
+		$migrationFilePath = pathresolve(self::$pathMigrations, self::$migrationFile);
 		$dir = opendir(self::$pathMigrations);
 		$files = [];
 
 		while ($file = readdir($dir)) {
 			$filePath = pathresolve(self::$pathMigrations, $file);
 
-			if ($file !== '.' && $file !== '..' && is_file($filePath) && $filePath !== 'migrations.lock' && extname($file) === '.php') {
+			if ($file !== '.' && $file !== '..' && is_file($filePath) && $filePath !== self::$migrationFile && extname($file) === '.php') {
 				$index = (int) $file;
 
 				$files[$index] = $file;
@@ -41,46 +41,50 @@ class Migrations {
 
 		ksort($files);
 
+		$hasChanges = false;
+
 		foreach ($files as $index => $file) {
-			$filePath = pathresolve(self::$pathMigrations, $file);
-
 			if (array_search($file, self::$migrations) === false) {
-				self::$migrations[] = $file;
-
-				require_once $filePath;
-
-				$file = fopen($lockFile, 'w');
-				fputs($file, json_encode([
-						'executed' => self::$migrations
-					], JSON_PRETTY_PRINT)
-				);
-				fclose($file);
+				$hasChanges = true;
 			}
 		}
-	}
 
-	private static function doMigration($name) {
-		$filePath = pathresolve(self::$pathMigrations, $name . '.php');
+		if ($hasChanges) {
+			$lockFile = fopen(pathresolve(self::$pathMigrations, self::$migrationLock), 'w');
+			fclose($lockFile);
 
-		if (file_exists($filePath)) {
-			require_once $filePath;
-		} else {
-			die('Migration file ' . $filePath . ' does not exists');
+			foreach ($files as $index => $file) {
+				if (array_search($file, self::$migrations) === false) {
+					$filePath = pathresolve(self::$pathMigrations, $file);
+					self::$migrations[] = $file;
+
+					require_once $filePath;
+
+					$file = fopen($migrationFilePath, 'w');
+					fputs($file, json_encode([
+							'executed' => self::$migrations
+						], JSON_PRETTY_PRINT)
+					);
+					fclose($file);
+				}
+			}
+
+			unlink(pathresolve(self::$pathMigrations, self::$migrationLock));
 		}
 	}
 
 	private static function preserveLastMigration() {
-		$lockFile = pathresolve(self::$pathMigrations, 'migrations.lock');
+		$migrationFilePath = pathresolve(self::$pathMigrations, self::$migrationFile);
 
-		if (file_exists($lockFile)) {
+		if (file_exists($migrationFilePath)) {
 			try {
-				$migrations = json_decode(file_get_contents($lockFile), true);
+				$migrations = json_decode(file_get_contents($migrationFilePath), true);
 				self::$migrations = $migrations['executed'];
 			} catch (Exception $e) {
 			}
 		} else {
 			try {
-				$file = fopen($lockFile, 'w');
+				$file = fopen($migrationFilePath, 'w');
 				fputs(
 					$file,
 					json_encode([
@@ -89,7 +93,7 @@ class Migrations {
 				);
 				fclose($file);
 			} catch (Exception $e) {
-				throw new \Exception('There is no write permission for lock file by path: ' . $lockFile . '. Create it manually and give permission for write.');
+				throw new \Exception('There is no write permission for lock file by path: ' . $migrationFilePath . '. Create it manually and give permission for write.');
 			}
 		}
 	}
